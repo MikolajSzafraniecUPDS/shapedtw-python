c59b852646d21415ba91d448bd7bbab43eac9661
MikolajSzafraniecUPDS <mikolaj.szafraniec.upds@gmail.com>
2023-05-24 01:01:13 +0200

    Added docstrings for all classes stored in shapeDescriptors module

--- shapedtw/shapeDescriptors.py
+++ shapedtw/shapeDescriptors.py
@@ -19,14 +19,15 @@
 
 """Classes representing hape descriptors, which are the key koncept of shape dtw algorithm"""
 
+import numpy as np
+
 from pywt import Wavelet, wavedec
 from abc import abstractmethod, ABC
 from numpy import ndarray
 from scipy.stats import linregress
 from shapedtw.exceptions import *
 from itertools import repeat
-from shapedtw.exceptions import SubsequenceShorterThanWindow
-
+from typing import List
 
 class ShapeDescriptor(ABC):
     """
@@ -133,8 +134,8 @@ class RawSubsequenceDescriptor(ShapeDescriptor):
 class PAADescriptor(ShapeDescriptor):
 
     """
-    Piecewise aggregation approximation is an y-shift variant shape descriptor. Given subsequence is split
-    into m equally length chunks. For each of the chunks mean values of temporal points falling within
+    Piecewise aggregation approximation is an y-shift dependent shape descriptor. Given subsequence is split
+    into m equally length chunks (windows). For each of the chunks mean values of temporal points falling within
     an interval is calculated and a vector af mean values is used as a shape descriptor.
 
     Length of intervals is specified by "piecewise_aggregation_window" argument provided in the class
@@ -179,7 +180,7 @@ class PAADescriptor(ShapeDescriptor):
 
         Parameters
         ---------------
-        :param ts_subsequence: Input time serie subsequence as numpy array
+        :param ts_subsequence: Input subsequence of a time series as a numpy array
 
         Returns
         ---------------
@@ -203,14 +204,43 @@ class DWTDescriptor(ShapeDescriptor):
     coefficients of the third level are concatenated to form the
     approximation, which is used the shape descriptor di of si, i.e.,
     F(·) = DWT, di = DWT (si).'
+
+    Attributes
+    ---------------
+    wave_type: str:
+        Type of wavelet basis (haar as default, according to Zhao and Itti)
+    mode: str:
+        Signal extension mode
+    level: int:
+        Decomposition level
     """
 
     def __init__(self, wave_type: str = "haar", mode: str = "symmetric", level: int = 3):
+        """
+        Constructs a DWTDescriptor object
+
+        Parameters
+        ---------------
+        :param wave_type: Type of wavelet basis (haar as default, according to Zhao and Itti)
+        :param mode: Signal extension mode. More details: https://pywavelets.readthedocs.io/en/latest/ref/signal-extension-modes.html#ref-modes
+        :param level: Decomposition level. More details: https://pywavelets.readthedocs.io/en/latest/ref/dwt-discrete-wavelet-transform.html
+        """
         self.wave_type = wave_type
         self.mode = mode
         self.level = level
 
     def get_shape_descriptor(self, ts_subsequence: ndarray) -> ndarray:
+        """
+        Get DWT shape descriptor for given subsequence
+
+        Parameters
+        ---------------
+        :param ts_subsequence: Input subsequence of a time series as a numpy array
+
+        Returns
+        ---------------
+        :return: DWT shape descriptor as a numpy array
+        """
         wavelet = Wavelet(self.wave_type)
         coefs_list = wavedec(ts_subsequence, wavelet, mode=self.mode, level=self.level)
         dwt_descriptor = np.concatenate(coefs_list)
@@ -222,7 +252,7 @@ class SlopeDescriptor(ShapeDescriptor):
     """
     Slope descriptor is a shape descriptor that is invariant to y-shift. It means, that
     two subsequences of the same shape which values are shifted on y axis by some delta
-    will be characterized by equal descriptors despite this difference.
+    will be characterized by the same descriptor despite this difference.
 
     Definition after Zhao and Itti:
     'Given a l-dimensional subsequence si, it is divided into m (m ≤ l)
@@ -232,20 +262,60 @@ class SlopeDescriptor(ShapeDescriptor):
     of the fitted lines from all intervals, we obtain a m-dimensional
     vector representation, which is the slope representation of si, i.e.,
     F(·) = Slope, di = Slope(si).'
+
+    Attributes
+    ---------------
+    slope_window: int:
+        width of a single interval (window) on which slope will be calculated
     """
 
     def __init__(self, slope_window: int = 2):
+        """
+        Constructs a SlopeDescriptor object
+
+        Parameters
+        ---------------
+        :param slope_window: width of a single interval (window) on which slope will be calculated
+
+        Raises
+        ---------------
+        :raise WrongSlopeWindow: Slope window need to be integer greater than 1,
+            otherwise this exception will be raised
+        """
         if not self._is_slope_correct(slope_window):
             raise WrongSlopeWindow(slope_window)
         self.slope_window = slope_window
 
     @staticmethod
     def _is_slope_correct(slope_window):
+        """
+        Check whether slope window is an integer greater than 1, as expected
+
+        Parameters
+        ---------------
+        :param slope_window: slope window provided by a user
+
+        Returns
+        ---------------
+        :returns: Bool - result of a check
+        """
         slope_correct = isinstance(slope_window, int) and slope_window > 1
         return slope_correct
 
     @staticmethod
     def _get_single_slope(input_vector: ndarray) -> float:
+        """
+        Get a value of slope for single window as a result of linear
+        regression
+
+        Parameters
+        ---------------
+        :param input_vector: single window of subsequence as a numpy array
+
+        Returns
+        ---------------
+        :return: value of slope for given window
+        """
         vector_length = len(input_vector)
         if vector_length == 1:
             return float(0)
@@ -255,10 +325,33 @@ class SlopeDescriptor(ShapeDescriptor):
 
     @staticmethod
     def _get_windows_slopes(windows: List[ndarray]) -> ndarray:
+        """
+        Calculate slopes for all windows
+
+        Parameters
+        ---------------
+        :param windows: list of subsequence's windows (intervals) as described
+            in descriptor's definition
+
+        Returns
+        ---------------
+        :return: slopes as a numpy array
+        """
         windows_slopes = np.array([SlopeDescriptor._get_single_slope(window) for window in windows])
         return windows_slopes
 
     def get_shape_descriptor(self, ts_subsequence: ndarray) -> ndarray:
+        """
+        Calculate slope shape descriptor for given subsequence
+
+        Parameters
+        ---------------
+        :param ts_subsequence: input subsequence of time series
+
+        Returns
+        ---------------
+        :return: slope shape descriptor as a numpy array
+        """
         windows = self._split_into_windows(ts_subsequence, self.slope_window)
         slope_descriptor = self._get_windows_slopes(windows)
 
@@ -276,21 +369,76 @@ class DerivativeShapeDescriptor(ShapeDescriptor):
     Dynamic Time Warping (E. Keogh and M. Pazzani. Derivative dynamic time warping. In SDM,
     volume 1, pages 5–7. SIAM, 2001.) (dDTW), we follow their formula to
     compute numeric derivatives.'
+
+    Exact formula for calculating derivative shape descriptor is to find
+    in aforementioned paper available online (on 24.05.2023):
+    https://www.ics.uci.edu/~pazzani/Publications/sdm01.pdf
     """
 
     @staticmethod
     def _get_first_order_diff(ts_subsequence: ndarray) -> ndarray:
+        """
+        Get first order differences for input subsequence
+
+        Parameters
+        ---------------
+        :param ts_subsequence: input subsequence as a numpy array
+
+        Returns
+        ---------------
+        :return: first order difference for given subsequence as a numpy array
+        """
         return ts_subsequence[1:] - ts_subsequence[:-1]
 
     @staticmethod
     def _get_second_order_diff(ts_subsequence: ndarray) -> ndarray:
+        """
+        Get second order differences for input subsequence
+
+        Parameters
+        ---------------
+        :param ts_subsequence: input subsequence as a numpy array
+
+        Returns
+        ---------------
+        :return: second order difference for given subsequence as a numpy array
+        """
         return (ts_subsequence[2:] - ts_subsequence[:-2]) / 2
 
     @staticmethod
     def _get_derivative(first_order_diff: ndarray, second_order_diff: ndarray) -> ndarray:
+        """
+        Calculate derivative for whole subsequence based on first and second order diff
+        vectors
+
+        Parameters
+        ---------------
+        :param first_order_diff: first order differences of input subsequence
+        :param second_order_diff: second order differences of input subsequence
+
+        Returns
+        ---------------
+        :return: derivative for whole subsequence
+        """
         return (first_order_diff[:-1] + second_order_diff) / 2
 
     def get_shape_descriptor(self, ts_subsequence: ndarray) -> ndarray:
+        """
+        Calculate derivative shape descriptor for given subsequence
+
+        Parameters
+        ---------------
+        :param ts_subsequence: input subsequence as a numpy array
+
+        Raises
+        ---------------
+        :raise SubsequenceTooShort: in order to calculate derivative shape descriptor input
+            subsequence must be of length at least 3 (width=1).
+
+        Returns
+        ---------------
+        :returns: derivative shape descriptor as a numpy array
+        """
         subsequence_length = len(ts_subsequence)
         if subsequence_length < 3:
             raise SubsequenceTooShort(subsequence_size=subsequence_length, min_required=3)
@@ -308,10 +456,31 @@ class CompoundDescriptor(ShapeDescriptor):
     Compound shape descriptor is a simple concatenation of provided shape descriptors. It is possible
     to specify a weights for each of them - it is worth to do if scales of values of chosen descriptors
     differs significantly.
+
+    Attributes
+    ---------------
+    shape_descriptors: List[ShapeDescriptor]:
+        list of shape descriptors
+    descriptors_weights: List[float]:
+        list of descriptors weights
     """
 
     def __init__(self, shape_descriptors: List[ShapeDescriptor], descriptors_weights: List[float] = None):
+        """
+        Constructs a CompoundDescriptor object
 
+        Parameters
+        ---------------
+        :param shape_descriptors: list of shape descriptors (instances of classes which inherits after ShapeDescriptor class)
+        :param descriptors_weights: list of weights for all given descriptors. It will be equal to 1 for all
+            descriptors as a default.
+
+        Raises
+        ---------------
+        :raise WrongWeightsNumber: length of weights list must be equal to length of descriptors list
+        :raise NotShapeDescriptor: at least one of objects provided in shape descriptors list is not
+            an instance of ShapeDescriptor class (or instance of child class).
+        """
         descriptors_number = len(shape_descriptors)
 
         if descriptors_weights is None:
@@ -330,6 +499,17 @@ class CompoundDescriptor(ShapeDescriptor):
         self.descriptors_weights = descriptors_weights
 
     def _calc_descriptors(self, ts_subsequence: ndarray) -> List[ndarray]:
+        """
+        Calculate shape descriptors, taking weights into account
+
+        Parameters
+        ---------------
+        :param ts_subsequence: input subsequence of time series as a numpy array
+
+        Returns
+        ---------------
+        :return: list of calculated shape descriptors
+        """
         descriptors_list = [
             descriptor.get_shape_descriptor(ts_subsequence) * weight for
             (descriptor, weight) in
@@ -339,6 +519,18 @@ class CompoundDescriptor(ShapeDescriptor):
         return descriptors_list
 
     def get_shape_descriptor(self, ts_subsequence: ndarray) -> ndarray:
+        """
+        Calculate shape descriptors and concat them into a single
+        compound descriptor
+
+        Parameters
+        ---------------
+        :param ts_subsequence: input subsequence of time series as a numpy array
+
+        Returns
+        ---------------
+        :return: compound shape descriptor as a numpy array
+        """
         descriptors_list = self._calc_descriptors(ts_subsequence)
         compound_descriptor = np.concatenate(descriptors_list)
 
