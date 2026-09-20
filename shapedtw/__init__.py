cf55252b1e2326b8b9d66bc17cbeea653c9fb334
Mikolaj Szafraniec <mszafra4@intercars.eu>
2026-09-20 18:06:35 +0200

    Cleaned up packaging mechanism and adapted it to newest requirements

--- shapedtw/__init__.py
+++ shapedtw/__init__.py
@@ -4,7 +4,7 @@ An extension to the dtw-python package, implementing a variant of the algorithm 
 
 __author__ = """Mikołaj Szafraniec"""
 __email__ = 'mikolaj.szafraniec.upds@gmail.com'
-__version__ = '1.0.3'
+__version__ = '1.1.0'
 
 # Imports which will be make after call 'from shapedtw import *'
 from shapedtw.shapedtw import shape_dtw
